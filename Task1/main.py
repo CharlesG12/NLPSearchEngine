b248bf283478ce6d52a8dc202393feb015dbfbb7
Charles Guo <charles.cheng.guo@gmail.com>
2019-04-08 16:45:34 -0500

    add wordnet

--- Task1/main.py
+++ Task1/main.py
@@ -1,6 +1,7 @@
 import os
 from nltk.stem import WordNetLemmatizer
 from nltk.parse.corenlp import CoreNLPParser
+from nltk.corpus import wordnet as wn
 import nltk
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
@@ -30,18 +31,30 @@ for i in range(1):
         for j in range(len(tokens)):
             lemma = lemmatizer.lemmatize(tokens[j])
             lemmatizes.append(lemma)
+            for synset in wn.synsets(lemma):
+                print(synset)
+                print()
+                nyms = ['hypernyms', 'hyponyms', 'part_meronyms', 'part_holonyms']
+                for n in nyms:
+                    try:
+                        print(n, end=" ")
+                        print(getattr(synset, n)())
+                    except AttributeError as e:
+                        print(e)
+                        pass
 
+        lemma_sent = " ".join(lemmatizes)
         tagged = nltk.pos_tag(lemmatizes)
-        parser = CoreNLPParser(url='http://localhost:9000')
-        next(parser.raw_parse(sentences[i])).pretty_print()
+        # parser = CoreNLPParser(url='http://localhost:9000')
+        # next(parser.raw_parse(lemma_sent)).pretty_print()
 
-        print("TOKENS:   ")
-        print(tokens)
-        print()
-        print("LEMMA:   ")
-        print(lemmatizes)
-        print()
-        print("TAG:     ")
-        print(tagged)
-        print()
+        # print("TOKENS:   ")
+        # print(tokens)
+        # print()
+        # print("LEMMA:   ")
+        # print(lemmatizes)
+        # print()
+        # print("TAG:     ")
+        # print(tagged)
+        # print()
 
