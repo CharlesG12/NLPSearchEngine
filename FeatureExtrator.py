20f09bf74d7fbc5afa5badde8ea6d021dbd31b03
Charles Guo <charles.cheng.guo@gmail.com>
2019-04-16 18:02:31 -0500

    cleaning

--- FeatureExtrator.py
+++ FeatureExtrator.py
@@ -3,15 +3,20 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 from nltk.parse.corenlp import CoreNLPParser
+from pprint import pprint
 
 
-class FeatherExtractor:
+class FeatureExtractor:
     path = ""
     files = []
     info = {}
 
     def __init__(self, _path):
         self.path = _path
+
+    def write(self):
+        for article, value in self.info.items():
+            print()
 
     def read_files(self):
         self.files = os.listdir(self.path)
@@ -34,18 +39,22 @@ class FeatherExtractor:
         dict_lemma = {}
         dict_tag = {}
         dict_parser = {}
-        for j in range(len(_sentences)):
+        dict_wordnet = {}
+        # for j in range(len(_sentences)):
+        for j in range(20):
+            print("processing tokens")
             dict_token[j] = nltk.word_tokenize(_sentences[j])
-            # print(_tokens)
+            print("processing lemma")
             dict_lemma[j] = self.lemma_generator(dict_token[j])
-            # print(_lemmas)
+            print("processing tagging")
             dict_tag[j] = nltk.pos_tag(dict_lemma[j])
-            # print(_tagged)
+            print("processing parsering")
             dict_parser[j] = self.parser_generator(dict_lemma[j])
-
-            # for k in range(len(dict_lemma[j])):
-            #     self.sysnet_generator(dict_lemma[j][k])
-        return {"token": dict_token, "lemma": dict_lemma, "tag": dict_tag, "parser": dict_parser}
+            # self.print_parser(dict_parser[j])
+            print("processing sysnet")
+            self.sysnet_generator(dict_lemma[j], dict_wordnet)
+        return {"token": dict_token, "lemma": dict_lemma, "tag": dict_tag,
+                "parser": dict_parser, "wordnet_feather": dict_wordnet}
 
     @staticmethod
     def lemma_generator(_tokens):
@@ -56,19 +65,24 @@ class FeatherExtractor:
             lemmas.append(lemma)
         return lemmas
 
+    def sysnet_generator(self, sentence, dict_wordnet):
+        for k in range(len(sentence)):
+            self.word_sysnet(sentence[k], dict_wordnet)
+
     @staticmethod
-    def sysnet_generator(_word):
+    def word_sysnet(_word, dict_wordnet):
         for synset in wn.synsets(_word):
-            print()
-            print(synset)
-            nyms = ['hypernyms', 'hyponyms', 'part_meronyms', 'part_holonyms']
-            for n in nyms:
+            nyms = {"hypernyms": [], "hyponyms": [], "part_meronyms": [], "part_holonyms": []}
+            # for n in nyms:
+            for attr, value in nyms.items():
                 try:
-                    print(n, end=" ")
-                    print(getattr(synset, n)())
+                    value = getattr(synset, attr)()
+                    if len(value) > 0:
+                        nyms[attr] = [o._name for o in value]
                 except AttributeError as e:
                     print(e)
                     pass
+            dict_wordnet[synset._name] = nyms
 
     @staticmethod
     def parser_generator(_words):
@@ -77,8 +91,13 @@ class FeatherExtractor:
         # next(parser.raw_parse(lemma_sent)).pretty_print()
         return parser.raw_parse(lemma_sent)
 
+    @staticmethod
+    def print_parser(_parser):
+        next(_parser).pretty_print()
+
 
 if __name__ == "__main__":
     extractor = FeatherExtractor("WikipediaArticles")
     extractor.read_files()
+    # extractor.write()
     print()
